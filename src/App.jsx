8a0bb6fc7a458a83b51c3e93524c7009dabb47d8
unknown <zemigz007@gmail.com>
2023-11-24 13:40:53 +0800

    creating hero section and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      This is my Portfolio Website!
-    </h1>   
+      <Navbar />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
